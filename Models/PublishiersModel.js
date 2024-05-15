66ea32fcf43feb2474c4e4a7baec6cdeafdc5fd3
FatimaZohraaaa <elhasnaouofatimazohra1@gmail.com>
2024-05-15 10:39:38 +0100

    first commit

---
+++ Models/PublishiersModel.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const publisherSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+const PublishersModel = mongoose.model('Publisher', publisherSchema);
+module.exports = PublishersModel;
